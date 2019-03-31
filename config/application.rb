55338490baf5dec9e9fbc69a9082b1984cdc1f57
Sota Takahashi <sotq17@gmail.com>
2019-03-31 17:19:24 +0900

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,10 @@ module ChatSpace
        g.javascripts false
        g.helper false
        g.test_framework false
-       config.time_zone = 'Asia/Tokyo'
+       config.time_zone = 'Tokyo'
        config.i18n.default_locale = :ja
        config.active_record.default_timezone = :local
-   end 
+   end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are autsomatically loaded.
